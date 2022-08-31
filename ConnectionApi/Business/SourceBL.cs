d6ce40d032130fff091a9baa858b53369a85a19c
Juan Rincon <juancarlos.rincon@metlife.com.co>
2022-08-31 08:27:08 -0500

    Nuevo controlador para la administracion del funcionamiento del motor.

---
+++ ConnectionApi/Business/SourceBL.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConnectionApi.Business
+{
+    public class SourceBL
+    {
+    }
+}
---
+++ ConnectionApi/Controllers/SourceController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+using System.Threading.Tasks;
+using ConnectionApi.Utils;
+using ConnectionApi.Modelos;
+using ConnectionApi.Context;
+using AppContext = ConnectionApi.Context.AppContext;
+using ConnectionApi.Business;
+
+namespace ConnectionApi.Controllers
+{
+    public class SourceController : Controller
+    {
+
+        private readonly IWebHostEnvironment _env;
+        private readonly WebContext _appContext;
+        public SourceController(IWebHostEnvironment env, WebContext appContext)
+        {
+            _env = env;
+            _appContext = appContext;
+        }
+
+        [HttpPost, Route("ManagementSource")]
+        public IActionResult ManagementSource(string accion)
+        {
+            try
+            {
+                SourceBL ManagementSource = new SourceBL(_env, _appContext);
+                var respuesta = ManagementSource.ManagementSource(accion);
+                return new ObjectResult(respuesta);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(AdministrarExcepcion(ex));
+            }
+        }
+
+        
+    }
+}
